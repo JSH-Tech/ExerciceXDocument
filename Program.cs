06ece4b91e6ee795e3b6e8a4efad84ff3f916a45
JSH-Tech <josueabots@gmail.com>
2024-08-21 00:51:36 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Xml.Linq;
+
+namespace ExerciceXDocument
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var doc = XDocument.Load(@"C:\Users\abots\source\repos\ExerciceXDocument\fichier.xml");
+            var personne = new Personne();
+
+            if (doc.Root is not null)
+            {
+                foreach (var item in doc.Root.Elements())
+                {
+                    if (item.Name=="Nom")
+                    {
+                        personne.Nom=item.Value;
+                    }
+                    else if (item.Name=="Prenom")
+                    {
+                        personne.Prenom=item.Value;
+                    }
+                    else if (item.Name=="DateDeNaissance")
+                    {
+                        personne.DateDeNaissance= DateTime.Parse(item.Value);
+                    }
+                    else if (item.Name=="Taille")
+                    {
+                        personne.Taille=int.Parse(item.Value);
+                    }
+                }
+            }
+
+            Console.WriteLine($"{personne.Nom} {personne.Prenom} {personne.DateDeNaissance} {personne.Taille}");
+
+            var address = new XElement("Adress", "487 rue de neuchatel");
+
+            if (doc.Root is not null)
+            {
+                var recupElement = doc.Root.Element("Taille");
+                if(recupElement is not null)
+                {
+                    recupElement.AddAfterSelf(address);
+                }
+            }
+            Console.WriteLine(doc.ToString());
+        }
+    }
+}
